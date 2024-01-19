9a3ad12454b8022ad639689b1dae12c632864448
Jack <jack.w.sibert@outlook.com>
2024-01-19 06:59:49 -1000

    Added Appbar, logo, scalability for logo and subtext, as well as styling in Theme.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
+      <ResponsiveAppBar/>
       Hello World
     </div>
   );
---
+++ client/src/components/Theme.jsx
@@ -0,0 +1,43 @@
+import { createTheme } from '@mui/material/styles';
+import '../styles/fonts.css';
+
+const Theme = createTheme({
+    palette: {
+        primary: {
+            main: '#342A6D', // Define your primary color
+            second: '#F4F3F9',
+            third: '#7C79A3',
+            fourth: '#E2E8EC',
+            fifth: '#9BB7B6'
+        }
+    },
+
+    typography: {
+        fontFamily: 'Roboto, sans-serif', // Default font
+        logo: 'KaushanScript, sans-serif',
+        logoLink: {
+            textDecoration: 'none',
+            padding: '1%',
+            fontSize: '3vw',
+        },
+        logoLinkSubtext:{
+            color: (theme) => theme.palette.primary.main,
+            display: 'flex',
+            alignItems: 'flex-end',
+            fontSize: '.75vw',
+        },
+    },
+    appBar: {
+        backgroundColor: 'white',
+        width: '100%',
+        padding: '1%'
+    },
+    appBarLinks: {
+        color: (theme) => theme.palette.primary.main,
+        display: 'flex',
+        fontWeight: 'bold',
+    }
+    // Add other customizations as needed
+});
+
+export default Theme; 
